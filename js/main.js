494d0789699925367bc70a7d0eae59cb19c36bae
suctuk <seuc9452@colorado.edu>
2023-11-04 14:28:13 -0600

    added some mad libs and updated the buttons to be pg/pg-13

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
+const customEvent = document.getElementById('customevent');
+const customVerb = document.getElementById('customverb');
+const customAdjective = document.getElementById('customadjective')
 
 
-let storyText = "I can't believe it Bob! I don't remember the last time :insertx: was this excited for :userinput:. They were practically :userinput: the last time this happened. Besides us being a bit woozy, :insertx: and I could clearly see :userinput:, and it was :inserty:. \"Knucklehead, how many times do I have to tell you! :insertz:\"";
+let storyText = "\"I can't believe it Bob!\" I remember the last time :insertx: was this excited for the concert. They were practically peeing their pants the last time this happened. Besides us being a bit woozy, :insertx: and I could clearly see :inserty:, and it was crazy. \"Knucklehead, how many times do I have to tell you! :insertz:!\"";
 
-const insertX  = ["Willy the Goblin","Big Daddy","Father Christmas"];
+const insertX  = ["Big Daddy", "the pope", "my dad", "your mom", "that guy frank", "the gays", "my math teacher becky", "the devil", "a turtle"];
 
-const insertY = ["the soup kitchen", "Disneyland", "the White House"];
+const insertY = ["the soup kitchen", "the stage", "the man's toilet", "the crowd of children", "the HUUUUUUUUGE banana", "Disneyland", "the White House"];
 
-const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+const insertZ = ["Spontaneously combuste", "Melt into a puddle on the sidewalk", "Turn into a slug and crawl away", "They don't know you like I do", "A majority of them have never done drugs", "The best people know who Shakira is", "I have a rock in my pocket", "Vsauce licked a chair", "I would die for a gallon of spianch"];
 
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
@@ -36,12 +39,23 @@ function result() {
     const name = customName.value;
     newStory = newStory.replaceAll('Bob', name);
   }
+  if(customEvent.value !== '') {
+    const event = customEvent.value;
+    newStory = newStory.replaceAll('the concert', event);
+  }
+  if(customVerb.value !== '') {
+    const verb = customVerb.value;
+    newStory = newStory.replaceAll('peeing their pants', verb);
+  }
+  if(customAdjective.value !== '') {
+    const adjective = customAdjective.value;
+    newStory = newStory.replaceAll('crazy', adjective);
+  }
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300/14) + ' stone';
-    const temperature =  Math.round((94-32)*(5/9)) + ' centigrade';
-    newStory = newStory.replaceAll('300 pounds', weight);
-    newStory = newStory.replaceAll('94 fahrenheit', temperature);
+  if(document.getElementById("pg-13").checked) {
+    newStory = newStory.replaceAll('this excited', 'flipping shit');
+    newStory = newStory.replaceAll('Knucklehead', 'Dipshit');
+    newStory = newStory.replaceAll('a bit woozy', 'fuckin wasted');
 
   }
 
