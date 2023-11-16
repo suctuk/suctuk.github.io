81cd5d1e4c45eebceadb08193eafee4717522d0b
suctuk <seuc9452@colorado.edu>
2023-11-16 13:01:20 -0700

    set up files for wa12

---
+++ js/wa12.js
@@ -0,0 +1,61 @@
+const newBtn = document.querySelector('#js-new-quote').addEventListener('click', getQuote);
+const answerBtn = document.querySelector('#js-tweet').addEventListener('click', displayAnswer);
+
+const questionTxt = document.querySelector('#js-quote-text');
+let answerTxt = document.querySelector('#js-answer-text');
+
+let answer = '';
+
+// this is the endpoint for the API that we want to get a reponse from
+const endpoint = 'https://www.boredapi.com/api/activity';
+
+async function getQuote() {
+
+   // try -> tries something; if it returns an error, it puts us into the catch block
+    try {
+        const response = await fetch(endpoint);
+        // if !response.ok is "if the response ISN'T okay (status code 200)"
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
+
+        console.log(json);
+        
+        // JSON is a dictionary, which is like a list; we call specific pieces of information out based on the 'key' associated with that value
+        displayQuote(json['activity']);
+        type = json['type'];
+        answerTxt.textContent = '';
+    }
+    catch(err) {
+        console.log(err);
+        alert('Failed to fetch new quote');
+    }
+}
+
+// this function shows the question
+function displayQuote(question) {
+    questionTxt.textContent = question;
+}
+
+// this function shows the answer
+function displayAnswer() {
+    answerTxt.textContent = type;
+}
+
+// we run getQuote once when the script first loads to populate a question
+// when the page is loading
+getQuote();
+
+
+
+
+// {
+//     "activity": "Learn how to beatbox",
+//     "type": "recreational",
+//     "participants": 1,
+//     "price": 0,
+//     "link": "https://en.wikipedia.org/wiki/Beatboxing",
+//     "key": "8731710",
+//     "accessibility": 1
+//   }
