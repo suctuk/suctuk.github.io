360b7dce945238e94135d0488593abb6b00861e6
suctuk <seuc9452@colorado.edu>
2023-12-08 10:25:51 -0700

    most of js basic clock complete.

--- js-midterm/js-midterm.js
+++ js-midterm/js-midterm.js
@@ -1,8 +1,16 @@
 //from w3 schools/
+document.querySelector("#clock").addEventListener("click", timeToPhone);
+document.querySelector("#reset").addEventListener("click", resetBtn);
+document.querySelector("#submit").addEventListener("click", submitBtn);
+// reference the phone number locally, not over in json because it is hard to write to the file. Would basically be a database. 
+let href;
+
+let phoneNumber;
 
 function startTime() {
   const today = new Date();
   let h = today.getHours();
+  href = h;
   let m = today.getMinutes();
   let s = today.getSeconds();
   m = checkTime(m);
@@ -16,4 +24,19 @@ function checkTime(i) {
     i = "0" + i;
   } // add zero in front of numbers < 10
   return i;
+}
+
+function resetBtn() {
+  alert("Your Phone Number has been reset");
+      phoneNumber = "";
+}
+
+function submitBtn() {
+    alert("You Submitted your phone number!");
+}
+
+function timeToPhone() {
+      console.log(phoneNumber);
+      phoneNumber += href;
+      console.log(phoneNumber);
 }
