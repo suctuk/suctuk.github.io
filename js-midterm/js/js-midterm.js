63a49c1de22f6aeb3864d8a631520f29d695002d
suctuk <seuc9452@colorado.edu>
2023-12-11 23:28:32 -0700

    Styled it a bit and added special buttons

--- js-midterm/js/js-midterm.js
+++ js-midterm/js/js-midterm.js
@@ -1,11 +1,11 @@
 //from w3 schools/
 document.querySelector("#clock").addEventListener("click", timeToPhone);
 document.querySelector("#reset").addEventListener("click", resetBtn);
-document.querySelector("#submit").addEventListener("click", submitBtn);
-// reference the phone number locally, not over in json because it is hard to write to the file. Would basically be a database. 
-let href;
 
-let phoneNumber;
+// reference the phone number locally, not over in json because it is hard to write to the file. Would basically be a database. 
+let href = "0";
+
+let phoneNumber = "";
 
 function startTime() {
   const today = new Date();
@@ -13,8 +13,10 @@ function startTime() {
   href = h;
   let m = today.getMinutes();
   let s = today.getSeconds();
+
   m = checkTime(m);
   s = checkTime(s);
+
   document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
   setTimeout(startTime, 1000);
 }
@@ -29,14 +31,20 @@ function checkTime(i) {
 function resetBtn() {
   alert("Your Phone Number has been reset");
       phoneNumber = "";
-}
+      var currentNumber = document.getElementById("phone");
 
-function submitBtn() {
-    alert("You Submitted your phone number!");
+      currentNumber.textContent = "Phone Number Goes Here";
 }
 
 function timeToPhone() {
       console.log(phoneNumber);
       phoneNumber += href;
       console.log(phoneNumber);
+
+      var currentNumber = document.getElementById("phone");
+
+      currentNumber.textContent = phoneNumber;
+
+
+
 }
