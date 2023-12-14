1746232122bc6ec58f55667feab61008d18be507
suctuk <seuc9452@colorado.edu>
2023-12-14 11:04:27 -0700

    added submit edge cases

--- js-midterm/js/js-midterm.js
+++ js-midterm/js/js-midterm.js
@@ -1,8 +1,10 @@
 document.querySelector("#clock").addEventListener("click", timeToPhone);
 document.querySelector("#reset").addEventListener("click", resetBtn);
+document.querySelector("#submit").addEventListener("click", celebrate);
+
 // document.querySelector("#backspace").addEventListener("click", backspace);
 
-// reference the phone number locally, not over in json because it is hard to write to the file. Would basically be a database. 
+// reference the phone number locally, not over in json because it is hard to write to the file. Would basically be a database.
 let href = "0";
 
 let phoneNumber = "";
@@ -20,7 +22,8 @@ function startTime() {
   let alarmMinutes = 60 - m;
   let alarmSeconds = 60 - s;
 
-  document.getElementById("alarm").innerHTML = alarmMinutes + " min " + alarmSeconds + " sec";
+  document.getElementById("alarm").innerHTML =
+    alarmMinutes + " min " + alarmSeconds + " sec";
   document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
   setTimeout(startTime, 1000);
 }
@@ -35,10 +38,10 @@ function checkTime(i) {
 
 function resetBtn() {
   alert("Your Phone Number has been reset");
-      phoneNumber = "";
-      var currentNumber = document.getElementById("phone");
+  phoneNumber = "";
+  var currentNumber = document.getElementById("phone");
 
-      currentNumber.textContent = "Phone Number Goes Here";
+  currentNumber.textContent = "Phone Number Goes Here";
 }
 // function backspace(){
 //   var currentNumber = document.getElementById("phone");
@@ -59,26 +62,61 @@ function resetBtn() {
 //   console.log(shortenedNumber);
 // }
 
-
-
 //add a button to max the number to 10
- 
+
 //add a timer for the next hour
 
 function timeToPhone() {
-      console.log(phoneNumber);
-      phoneNumber += href;
-      console.log(phoneNumber);
-  
-      var currentNumber = document.getElementById("phone");
+  console.log(phoneNumber);
+  phoneNumber += href;
+  console.log(phoneNumber);
 
-      currentNumber.textContent = phoneNumber;
+  var currentNumber = document.getElementById("phone");
 
-      if(phoneNumber.length === 10){
-        alert("You have 10 digits! Hit submit if you are done!");
-      }
-      else if(phoneNumber.length > 10){
-        alert("ToO LoNG");
-      }
+  currentNumber.textContent = phoneNumber;
 
+  if (phoneNumber.length === 10) {
+    alert("You have 10 digits! Hit submit if you are done!");
+  } else if (phoneNumber.length > 10) {
+    alert("ToO LoNG");
+  }
+}
+
+//pulled from Balls In class assignment
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+//adapted from code from chatGPT
+function celebrate() {
+  if (phoneNumber.length < 10) {
+    alert("You do not have enough digits");
+    return;
+  } else if (phoneNumber.length > 10) {
+    alert("ToO LoNG");
+    return;
+  } else if(phoneNumber.length === 10){
+    const confettiContainer = document.createElement("div");
+    confettiContainer.classList.add("confetti-container");
+
+    for (let i = 0; i < 100; i++) {
+      const confetti = document.createElement("div");
+      confetti.classList.add("confetti");
+      confetti.style.left = Math.random() * window.innerWidth + "px";
+      confetti.style.animationDuration = Math.random() * 1 + 0.5 + "s";
+      confetti.style.backgroundColor = randomRGB();
+      confettiContainer.appendChild(confetti);
+    }
+
+    document.body.appendChild(confettiContainer);
+
+    // Remove confetti after animation duration
+    setTimeout(() => {
+      document.body.removeChild(confettiContainer);
+    }, 1500);
+  }
 }
