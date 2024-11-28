739aa083659b0f7defde217fb063d1b7c2fda1e3
suctuk <seuc9452@colorado.edu>
2024-11-27 23:15:25 -0700

    changed all colors to dark mode.  added a scroll button from hero

---
+++ draftUpdate/js/scrollDown.js
@@ -0,0 +1,6 @@
+// Add this JavaScript function
+function scrollToContent() {
+    // Replace 'main-content' with the ID of your main content section
+    const mainContent = document.getElementById('main-content');
+    mainContent.scrollIntoView({ behavior: 'smooth' });
+  }
